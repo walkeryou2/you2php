102520ba779cd7d650ed0549d64ca629ec86afdb
walkeryou2 <36576157+walkeryou2@users.noreply.github.com>
2018-02-22 01:26:53 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyC7yi0jh6lgfpdyhsaeX2kJ4xprIDYBGro');//这里改成您的api key
+define('GJ_CODE', 'HK');//国家代码
+define('SITE_NAME', 'Youtube');//网站名字
+define('TITLENAME', 'Youtube');//网站名字
+define('EN2DEKEY', 'AnQEF1XDEF535e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'xyxyeden78@gmail.com');//邮箱
+?>
